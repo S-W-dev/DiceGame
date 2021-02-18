60f44d82632e98f43d000ca6de444ed0fa828613
Walter Shewmake <25shewmwalj@washk12.org>
2021-02-17 18:24:04 -0700

    asdasd

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebSocketSharp;
 
 namespace DiceGame {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+
+            using (var ws = new WebSocket("ws://concretegames.net:667/socket/?EIO=2&transport=websocket")) {
+                Console.WriteLine(ws);
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Message: " + e.Data);
+                ws.OnError += (sender, e) =>
+                    Console.WriteLine("Error: " + e.Message);
+                ws.Connect();
+                Console.ReadKey(true);
+            }
+
         }
+
     }
 }

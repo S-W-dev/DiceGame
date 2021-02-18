99c207684f64987a5caa9a3daa7524e666435603
Spencer Smith <apricotseed101@gmail.com>
2021-02-18 14:43:46 -0700

    stuff

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,10 +23,22 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DiceGame.Properties.Resources.game_table;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -9,29 +9,74 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WebSocketSharp;
 using System.Threading;
+using Newtonsoft.Json;
 
 namespace DiceGame {
     public partial class Form1 : Form {
+        public static WebSocket server = null;
+        public static bool connected = false;
+        Thread conn;
         public Form1() {
             InitializeComponent();
 
-            Thread conn = new Thread(ServerComponents.connection);
-            Console.WriteLine("HI"); //Still says hi even though the server starts
+            TopMost = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            WindowState = FormWindowState.Maximized;
+            MaximizeBox = false;
+
+            conn = new Thread(ServerComponents.connection);
+            conn.Start();
+            while(!connected) {
+                Thread.Sleep(1);
+            }
+            ServerComponents.SendMessage(server, "Hello");
         }
 
+        protected override void WndProc(ref Message message) {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+
+            switch (message.Msg) {
+                case WM_SYSCOMMAND:
+                    int command = message.WParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                        return;
+                    break;
+            }
+
+            base.WndProc(ref message);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            server.Close();
+            conn.Abort();
+            Application.Exit();
+        }
     }
 
     class ServerComponents {
         public static void connection() {
             using (var ws = new WebSocket("ws://concretegames.net:667/socket/?EIO=2&transport=websocket")) {
+                Form1.server = ws;
                 Console.WriteLine(ws);
                 ws.OnMessage += (sender, e) =>
                     Console.WriteLine("Message: " + e.Data);
                 ws.OnError += (sender, e) =>
                     Console.WriteLine("Error: " + e.Message);
+                ws.OnOpen += (sender, e) =>
+                    Form1.connected = true;
                 ws.Connect();
                 Console.ReadKey(true);
             }
+        }
+
+        public static void SendMessage(WebSocket ws, string message) {
+            //ws.Send(JsonConvert.SerializeObject(new { type = "getframe" }));
+            ws.Send(message);
         }
     }
 }
